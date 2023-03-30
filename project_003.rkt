0bdb29bf14b8a78dbc370061fdd8de7a2f1283ba
Bjørn Warland <bwarland@gmail.com>
2023-03-30 19:48:23 +0100

    små funksjoner som manipulerer lister, mest kjærmkommen repetisjon fra HDTP

--- project_003.rkt
+++ project_003.rkt
@@ -24,9 +24,70 @@
 
 #lang racket
 
-;; create a list of prime numbers up to 
+;; in-list?: N (listof N) -> boolean
+;; is a given number in a list?
+
+(define (in-list? number a-list)
+  (cond
+    [(empty? a-list) #false]
+    [(= number (first a-list)) #true]
+    [else (in-list? number (rest a-list))]))
+
+;; (equal? (in-list? 1 '()) #false)
+;; (equal? (in-list? 1 (list 2 3 4)) #false)
+;; (equal? (in-list? 1 (list 1 2 3)) #true)
+;; (equal? (in-list? 1 (list 2 3 1)) #true)
+
+;; add-if-not-in: N (listof N) -> (listof N)
+;; add a number to a list if it is not in
+
+(define (add-if-not-in-list number a-list)
+  (cond
+    [(not (in-list? number a-list)) (cons number a-list)]
+    [else a-list]))
+
+;; (equal? (add-if-not-in-list 1 (list 2 3)) (list 1 2 3))
+;; (equal? (add-if-not-in-list 1 (list 1 3)) (list 1 3))
+
+;; ,----
+;; | divisible-by-numberslist: N (listof N) -> boolean
+;; | check if a number is divisible by all numbers in a list
+;; `----
+
+
+;; (define (divisible-by-numberslist number alon)
+;;   (cond
+;;     [(empty? alon) #false]
+;;     [(and (= (modulo number (first alon)) 0)
+;;           (divisible-by-numberslist number (rest alon))) #true] ;; denne fungerer ikke fordi
+;;                                                                 ;; den alltid vil slutte med #false
+;;     [else (divisible-by-numberslist number (rest alon))]))
+
+;; ,----
+;; | her kan jeg sikkert bruke andmap, men jeg burde også prøve å bygge prossessen opp
+;; | fra bunn av selv
+;; `----
+
+
+
+;; (equal? (divisible-by-numberslist 10 (list 1 2 5)) #true)
+;; (equal? (divisible-by-numberslist 10 (list 1 2 5 6)) #false)
+
+;; create a list of prime numbers up to N
+;; function for determining prime
 
 ;; prime?: N -> boolean
 ;; check if a number is a prime number
+
+;; (define (prime? number)
+;;   (define list-primes (list empty)
+;;   (cond
+;;     [(
+
+;; (equal? (prime? 2) #true)
+;; (equal? (prime? 3) #true)
+;; (equal? (prime? 5) #true)
+;; (equal? (prime? 7) #true)
+
 
 
